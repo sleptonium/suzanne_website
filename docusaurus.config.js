6c732c067b0e180e5f58d09fdf16345f492d2081
Sleptonium <76815323+sleptonium@users.noreply.github.com>
2024-08-31 11:00:58 +0100

    Update docusaurus.config.js
    More url changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/sleptonium/suzanne_website',
             label: 'GitHub',
             position: 'right',
           },
@@ -131,7 +131,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/sleptonium/suzanne_website',
               },
             ],
           },
