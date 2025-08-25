1ad0f3f271c617b0526eea9a45bf65d8f552cf05
sleptonium <sleptonium@risksim.co.uk>
2025-08-25 13:00:54 +0100

    fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,14 +52,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/sleptonium/suzanne_website',
+            'https://github.com/sleptonium/suzanne_website/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/sleptonium/suzanne_website',
+            'https://github.com/sleptonium/suzanne_website/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
